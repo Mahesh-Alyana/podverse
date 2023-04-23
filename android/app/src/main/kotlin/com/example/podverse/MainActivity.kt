f3250732686aba25fb62bcdcf23dab216625a652
Mahesh-Alyana <20211A6604@bvrit.ac.in>
2023-04-24 02:50:47 +0530

    project started

---
+++ android/app/src/main/kotlin/com/example/podverse/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.podverse
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/login/web.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class WebLoginScreen extends StatefulWidget {
+  const WebLoginScreen({super.key});
+
+  @override
+  State<WebLoginScreen> createState() => _WebLoginScreenState();
+}
+
+class _WebLoginScreenState extends State<WebLoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
